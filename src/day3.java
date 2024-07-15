5b7eba7447ce3dc423d0c78da7db91ef13a1b22e
r-ajan0 <rajanprime123@gmail.com>
2024-07-15 11:05:48 +0545

    day2 and day3

--- src/day3.java
+++ src/day3.java
@@ -23,34 +23,48 @@ public class day3 {
 //            System.out.println("Ram");
 //        }
 
-        int i = 1;
-        while (i <= 100) {
-            System.out.println(i);
-            i++;
+//        int i = 1;
+//        while (i <= 100) {
+//            System.out.println(i);
+//            i++;
+//        }
+//
+//        int j = 0;
+//        while (j < 100) {
+//            j++;
+//            System.out.println(j);
+//            j++;
+//        }
+//
+//        int k =1;
+//        while (k <= 100) {
+//            k++;
+//            System.out.println(k);
+//            k++;
+//        }
+//
+//        int userInput2 = input.nextInt();
+//        int l = 1;
+//
+//        while ( l<= userInput2){
+//            System.out.println("Ram");
+//            l++;
+//
+//        }
+
+        for(int i =1;i<=100;i++){
+            if(i%3==0 && i%5==0){
+                System.out.println("FizzBuzz");
+            }
+            else if(i%3==0){
+                System.out.println("Fizz");
+            }
+            else if(i%5==0){
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(i);
+            }
         }
-
-        int j = 0;
-        while (j < 100) {
-            j++;
-            System.out.println(j);
-            j++;
-        }
-
-        int k =1;
-        while (k <= 100) {
-            k++;
-            System.out.println(k);
-            k++;
-        }
-
-        int userInput2 = input.nextInt();
-        int l = 1;
-
-        while ( l<= userInput2){
-            System.out.println("Ram");
-            l++;
-
-        }
-
     }
 }

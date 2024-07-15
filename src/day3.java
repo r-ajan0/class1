9157527b296c2714fa5a74335a71e00f663c8ce3
r-ajan0 <rajanprime123@gmail.com>
2024-07-15 11:11:16 +0545

    day2 and day3

--- src/day3.java
+++ src/day3.java
@@ -16,7 +16,7 @@ public class day3 {
 //            System.out.println(i);
 //        }
 //
-        Scanner input = new Scanner(System.in);
+//        Scanner input = new Scanner(System.in);
 //        System.out.println("Enter an integer: ");
 //        int userInput = input.nextInt();
 //        for (int i = 1; i<= userInput; i++) {
@@ -52,19 +52,19 @@ public class day3 {
 //
 //        }
 
-        for(int i =1;i<=100;i++){
-            if(i%3==0 && i%5==0){
-                System.out.println("FizzBuzz");
-            }
-            else if(i%3==0){
-                System.out.println("Fizz");
-            }
-            else if(i%5==0){
-                System.out.println("Buzz");
-            }
-            else{
-                System.out.println(i);
-            }
-        }
+//        for(int i =1;i<=100;i++){
+//            if(i%3==0 && i%5==0){
+//                System.out.println("FizzBuzz");
+//            }
+//            else if(i%3==0){
+//                System.out.println("Fizz");
+//            }
+//            else if(i%5==0){
+//                System.out.println("Buzz");
+//            }
+//            else{
+//                System.out.println(i);
+//            }
+//        }
     }
 }
